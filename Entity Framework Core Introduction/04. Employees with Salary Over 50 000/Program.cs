d8ab48be757481c48139b97877f68ddbd5648b5d
Kalsito1017 <kurabiika34@abv.bg>
2023-03-27 22:51:31 +0300

    Addded 4 Exercises

---
+++ Entity Framework Core Introduction/04. Employees with Salary Over 50 000/Program.cs
@@ -0,0 +1,23 @@
+﻿using _03._Employees_Full_Information.Data.Models;
+using System.Text;
+
+namespace _04._Employees_with_Salary_Over_50_000;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+        string output = GetEmployeesWithSalaryOver50000(context);
+        Console.WriteLine(output);
+    }
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var employees = context.Employees.Where(x => x.Salary > 50000).OrderBy(x => x.FirstName).ToList();
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+        }
+        return sb.ToString().Trim();
+    }
+}
---
+++ Entity Framework Core Introduction/05. Employees from Research and Development/Program.cs
@@ -0,0 +1,24 @@
+﻿using _03._Employees_Full_Information.Data.Models;
+using System.Text;
+
+namespace _05._Employees_from_Research_and_Development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees.OrderBy(x => x.Salary).OrderByDescending(x => x.FirstName);
+            foreach (var item in employees)
+            {
+                sb.AppendLine();
+            }
+            return sb.ToString().Trim();
+            
+        }
+    }
+}
