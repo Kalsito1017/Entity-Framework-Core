6087d1545eedc4bfc0ea0baee15f624de35f740b
Kalsito1017 <kurabiika34@abv.bg>
2023-04-02 17:21:09 +0300

    Added  the full exercise

--- Entity Framework Core Introduction/05. Employees from Research and Development/Program.cs
+++ Entity Framework Core Introduction/05. Employees from Research and Development/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _03._Employees_Full_Information.Data.Models;
+using System;
 using System.Text;
 
 namespace _05._Employees_from_Research_and_Development
@@ -7,18 +8,22 @@ namespace _05._Employees_from_Research_and_Development
     {
         static void Main(string[] args)
         {
-            
+            SoftUniContext vs = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(vs));
         }
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
-            var employees = context.Employees.OrderBy(x => x.Salary).OrderByDescending(x => x.FirstName);
+            var employees = context.Employees
+                .OrderBy(x => x.Salary)
+                .OrderByDescending(x => x.FirstName)
+                .Where(x => x.Department.Name == "Research and Development");
             foreach (var item in employees)
             {
-                sb.AppendLine();
+                sb.AppendLine($"{item.FirstName} {item.LastName} from Research and Development - ${item.Salary:F2}");
             }
             return sb.ToString().Trim();
-            
+
         }
     }
 }
