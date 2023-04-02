b622459e83eef22d8ecbadaa3ff012deedf7f414
Kalsito1017 <kurabiika34@abv.bg>
2023-04-02 20:57:06 +0300

    Added Exercise 6

---
+++ Entity Framework Core Introduction/06. Adding a New Address and Updating Employee/Program.cs
@@ -0,0 +1,36 @@
+﻿using _03._Employees_Full_Information.Data.Models;
+using System.Text;
+
+namespace _06._Adding_a_New_Address_and_Updating_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext sb = new SoftUniContext();
+            Console.WriteLine(AddNewAddressToEmployee(sb));
+        }
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+            var nakovEmployee = context.Employees.First(e => e.LastName == "Nakov");
+            nakovEmployee.Address = address;
+            context.SaveChanges();
+            StringBuilder sb = new StringBuilder();
+            var adreses = context.Addresses
+                .OrderByDescending(x => x.AddressId)
+                .Select(x => new {x.AddressText})
+                .Take(10);
+            foreach (var item in adreses)
+            {
+                sb.AppendLine(item.AddressText);
+            }
+            return sb.ToString().Trim();
+               
+        }
+    }
+}
