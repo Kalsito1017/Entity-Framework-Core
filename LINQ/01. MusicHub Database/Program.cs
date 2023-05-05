cc246b9e9f579995188cf53e2488f9d74a40a4cd
Kalsito1017 <kurabiika34@abv.bg>
2023-05-05 22:00:33 +0300

    Added LINQ Exercise

---
+++ LINQ/01. MusicHub Database/Program.cs
@@ -0,0 +1,14 @@
+﻿using _01._MusicHub_Database.Data;
+
+namespace _01._MusicHub_Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MusicContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
