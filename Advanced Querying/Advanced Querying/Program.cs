f1aff9241f5ed008ade03e035b163d57ef04d177
Kalsito1017 <kurabiika34@abv.bg>
2023-08-26 15:35:49 +0300

    Added Advanced Querying/Program.cs and JSON Folder

--- Advanced Querying/Advanced Querying/Program.cs
+++ Advanced Querying/Advanced Querying/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            var pascalTriangle = new long[number][];
+            for (var row = 0; row < number; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;  // first element is 1
+                pascalTriangle[row][^1] = 1; // last element is 1
+
+                for (var col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            for (var row = 0; row < number; row++)
+            {
+                Console.WriteLine(String.Join(" ",pascalTriangle[row]));
+            }
+
         }
     }
 }
---
+++ JavaScript Object Notation - JSON/JavaScript Object Notation - JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JavaScript_Object_Notation___JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
