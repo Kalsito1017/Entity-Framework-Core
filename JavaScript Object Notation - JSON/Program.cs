686cd9f52366cc3c16fd2b218698582a4f7d100a
Kaloyan Stoichkov <98273563+Kalsito1017@users.noreply.github.com>
2023-10-02 20:07:36 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JavaScript_Object_Notation___JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
