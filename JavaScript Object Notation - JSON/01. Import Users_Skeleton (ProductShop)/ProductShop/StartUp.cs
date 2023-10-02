26815e1b915d93b402f4916ce88256d3a5bc4804
Kaloyan Stoichkov <98273563+Kalsito1017@users.noreply.github.com>
2023-10-02 23:36:28 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/01. Import Users_Skeleton (ProductShop)/ProductShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs;
+using ProductShop.Models;
+using System.Text.Json;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            var config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.AddProfile<ProductShopProfile>();
+                });
+            mapper = config.CreateMapper();
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            var result = ImportUsers(context, inputJson);
+            Console.WriteLine(result);
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var Dtousers = JsonConvert.DeserializeObject<IEnumerable<UserInputModel[]>>(inputJson);
+            var users = mapper.Map<IEnumerable<User>>(Dtousers);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+             return $"Successfully imported {users.Count()}";
+
+        }
+    }
+}
