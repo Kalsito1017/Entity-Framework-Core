3460a08a0d6eb8ad777f3184a8c675e26ceccde2
Kaloyan Stoichkov <98273563+Kalsito1017@users.noreply.github.com>
2023-10-03 22:01:39 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensible_Markup_Language___XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
