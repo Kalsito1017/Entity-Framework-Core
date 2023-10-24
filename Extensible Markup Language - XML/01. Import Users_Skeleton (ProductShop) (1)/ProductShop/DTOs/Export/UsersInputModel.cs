508de0d5537ad7a89e0220fe86d6ada57484fd54
Kaloyan Stoichkov <98273563+Kalsito1017@users.noreply.github.com>
2023-10-24 13:52:32 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/01. Import Users_Skeleton (ProductShop) (1)/ProductShop/DTOs/Export/UsersInputModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ProductShop.DTOs.Export
+{
+    public class UsersInputModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
